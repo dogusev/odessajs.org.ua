4668ce00707143ffcb7bddc9a0fa019bdd8fc5b0
Dmitriy Gusev <d.o.gusev@gmail.com>
2018-01-22 19:30:37 +0200

    Ojs 18 redesign (#36)

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ var config = {
 };
 firebase.initializeApp(config);
 
-function subscribeEmail(email) {
+function subscribeEmail(email, name) {
   var id = Date.now();
-  return firebase.database().ref('emails/' + id).set(email);
+  return firebase.database().ref('emails/' + id).set({email: email, name: name});
 }
 
 function validateEmail(email) {
@@ -21,6 +21,7 @@ function validateEmail(email) {
 
 $(document).ready(function() {
 
+/*
   $('.menu__list').on('click', '.menu__list-item', function() {
     $('body').animate({scrollTop: $('[data-section="'+ $(this).attr('data-target')+'"]').offset().top}, '500');
   });
@@ -32,12 +33,13 @@ $(document).ready(function() {
       $('.button-top').fadeOut(200);
     }
   });
+*/
 
   $('.reports__list-item-theme').click(function () {
     $(this).next().toggle();
   });
 
-  $(document).trigger('scroll');
+  /*$(document).trigger('scroll');
 
   $('.button-top').click(function(){
     $('body').animate({scrollTop: 0}, '500');
@@ -49,18 +51,20 @@ $(document).ready(function() {
   }, function(){
     var $img = $(this).find('img');
     $img.attr('src', $img.attr('data-grey-src'));
-  });
+  });*/
 
-  $('.subscribe-button').click(function() {
+  $('.subscribe_form').submit(function(e) {
+    e.preventDefault();
     var that = $(this);
     that.find('i').css('display', 'block');
     that.find('span').css('display', 'none');
-    var email = $('.subscribe-input').val();
+    var email = that.find('.subscribe-email-input').val();
+    var name = that.find('.subscribe-name-input').val();
     if (validateEmail(email)) {
-      subscribeEmail(email).then(function() {
+      subscribeEmail(email, name).then(function() {
         that.find('i').css('display', 'none');
         that.find('span').css('display', 'block');
-        $('.subscribe-input').val('');
+        that.find('.subscribe-input').val('');
         alert('Благодарим за подписку.');
       });
     } else {
@@ -71,6 +75,7 @@ $(document).ready(function() {
   });
 
 });
+/*
 
 
 
@@ -172,3 +177,64 @@ function init(){
 
     myMap.geoObjects.add(myPlacemark);
 }
+*/
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 57)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+})(jQuery); // End of use strict
