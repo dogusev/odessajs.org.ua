7257bf387dd5b44701e4420f1bc3d9599d5dc06a
Dmitriy Gusev <d.o.gusev@gmail.com>
2019-07-02 17:36:04 +0300

    [slider] init (#167)
    * [slider] init

--- vendor/infoSlider.js
+++ vendor/infoSlider.js
@@ -1,11 +1,93 @@
+$(document).ready(function () {
+    $(window).resize(function () {
+        $('#speakersSlider').not('.slick-initialized').slick('resize');
+    });
 
-$(document).ready(function(){
+    $(window).on('orientationchange', function () {
+        $('#speakersSlider').not('.slick-initialized').slick('resize');
+    });
+    $(window).resize(function () {
+        $('#workshopsSlider').not('.slick-initialized').slick('resize');
+    });
+
+    $(window).on('orientationchange', function () {
+        $('#workshopsSlider').not('.slick-initialized').slick('resize');
+    });
+
     $('.slider-wrapper').slick({
-        //infinite: true,
         arrows: true,
-        //slidesToShow: 2,
-        //slidesToScroll:1,
-        //variableWidth: true
         dots: true,
     });
+
+
+    $('#speakersSlider').slick({
+        mobileFirst: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        responsive: [
+
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+        ]
+    });
+    $('#workshopsSlider').slick({
+        mobileFirst: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        responsive: [
+
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+        ]
+    });
+
+
 });
