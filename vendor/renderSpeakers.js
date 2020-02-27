17675d02694886c4c900252c57ce1de8c93403e9
mkatrichenko <m_katrichenko@ukr.net>
2020-02-27 16:45:30 +0200

    [updates25/02] init

--- vendor/renderSpeakers.js
+++ vendor/renderSpeakers.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     var speakers = [
         {
-            image: "img-min/2020/speakers/VLADIMIR NOVIK.jpg",
+            image: "img-min/2020/speakers/vladimir_novic.jpg",
             name: "Vladimir Novick",
             speakerPos: [
                 {
@@ -15,34 +15,34 @@ $(document).ready(function () {
             rept: [
                 {
                     title: "Moving from JavaScript to ReasonML",
-                    description: "",
+                    description: "Created by Jordan Walke at Facebook, ReasonML is a syntax extension and toolchain for OCaml (functional programming language) that lets you write simple, fast, and quality type-safe code leveraging JavaScript and OCaml ecosystems. The cool part is that Reason looks very close to JavaScript, but it has functional languages constructs such as pattern matching, variants and amazing type system. In this talk we understand what benefits ReasonML gives us compared to JavaScript/TypeScript/Flow, why it's used in critical financial web apps, in Facebook Messenger and how we can start using it in our existing JavaScript projects using both familiar JS ecosystem we are so used to. There is a saying 'In Reason if it compiles - it works' - we will see why this is actually true and not just a saying.",
                     lang: "[EN]"
                 },
                 {
                     title: "Architecting scalable serverless applications with GraphQL api",
-                    description: "",
+                    description: "In this talk, we will see what are the benefits of building event-driven Realtime GraphQL backend and what problems it will solve. We will see how we can bootstrap our GraphQL backend fast with free and open-source Hasura engine and connect it to serverless functions for custom business logic, providing our web or mobile apps with infinitely scalable backend with modern GraphQL API.",
                     lang: "[EN]"
                 }
             ],
             aboutSpeaker: '',
             socialsRendered: '',
             socials: [
-                // {
-                //     link: 'https://github.com/lirantal',
-                //     fatype: 'github'
-                // },
-                // {
-                //     link: 'https://twitter.com/liran_tal',
-                //     fatype: 'twitter'
-                // },
-                // {
-                //     link: 'https://medium.com/@liran.tal',
-                //     fatype: 'link'
-                // },
+                {
+                    link: 'https://github.com/vnovick',
+                    fatype: 'github'
+                },
+                {
+                    link: 'https://twitter.com/VladimirNovick',
+                    fatype: 'twitter'
+                },
+                {
+                    link: 'https://www.linkedin.com/in/vnovick',
+                    fatype: 'linkedin'
+                },
             ],
         },
         {
-            image: "img-min/2020/speakers/JAMIE MARIA SCHOUREN.jpg",
+            image: "img-min/2020/speakers/jamie_maria_schouren.jpg",
             name: "Jamie Maria Schouren ",
             speakerPos: [
                 {
@@ -55,12 +55,30 @@ $(document).ready(function () {
             rept: [
                 {
                     title: "Software Architectures gone wild (Speech)",
-                    description: "",
+                    description: "Buckle-up and get ready for a (half) rush hour through the world of Software Architectures! Everyone who works in software has heard of terms like ‘monoliths’, ‘headless’ , ‘microservices’ and even ’service oriented architectures’. We all know what they are, but how much do we really know about them? In this half an hour, we will show you the differences, the similarities, when to use them, when not to use them, the success stories and of course: the massive failures. Get ready for a comedic blast, architecture is going to be fast and fun! " +
+                        "<h4> Plan and theses of your speech</h4>" +
+                        "The talk will be about different software architectures and how they evolved over time.I will talk about: monolithic architectures, headless, microservices and service oriented architectures.On all architectures I will explain what it is, how to use it, and also how not to use it.I will tell the advantages and the disadvantages per architecture and show examples of live websites and live software using it.The talk will be energetic, fast and fun.There will be no boring bullet point list, or a lot of text.There will be a lot of images, jokes, and real life examples.At the end I will show how to get started yourself with some GraphQL and of course javascript!" +
+                        "<h4> What is the value of your talk, on what do you want to inspire the listeners ?</h4>" +
+                        "I want to bring knowledge to the audience and inspire them to try new architectures.For years all developers are using the same because they are used to it, and the new architectures were only for ‘big companies’.Now everyone can use the new technologies, and they can start it today.One of the things I also would like to show is that architectures are not boring, they can be fun! I will show real life examples of things going well, and things going bad.It will be very visual and fun." +
+                        "<h4> What will your listeners take for themselves from your talk ?</h4>" +
+                        "It will be a very honest and fun talk.Software architectures are always very boring, and not something(front - end / javascript) developers want to be bother with.However in this talk I will try to make it fun, but also show the real side.In documentation it only highlights the good sides, never the ‘real’ sides." +
+                        "<h4> What will listeners take:</h4>" +
+                        "Listeners will get more knowledge and will be able to make a real good decision for the architecture for their(next) project, which will help them in the later process.",
                     lang: "[EN]"
                 },
                 {
                     title: "What the Hack?! Unicorns on Steroids (TechCorner)",
-                    description: "",
+                    description: "<h4>Summary:</h4>" +
+                        "Ever wondered how companies such as Facebook, Netflix and AirBnB were able to grow as fast as they did? How they started, what method they used? Growth hacking started as a strategy for early - stage startups who need massive growth in a short time on a(very) small budget, with a team of engineers and marketeers.In this fun and energetic talk Jamie Maria will explain the basics of Growth Hacking, what it is, how it is done, how the Unicorns we know today were able to get BIG in no - time - and most importantly, how you can growth hack your company!" +
+                        "<h4> Plan and theses of your speech </h4>" +
+                        "The talk will be about what growth hacking is, and especially how companies like facebook, uber, airbnb and others got super big in a very short time without money.Did you know Netflix was a company sending actual dvd’s by post ? And that airbnb got big because they ‘hacked’ craigslist and where able to place hundreds of ads without paying for it ? This talk will also show how marketing and developers work together to make the product super successful, how you can make your company successful without waiting for a ’sales person’ to go out and sell it.It is all about hacking, data and making sure you are in the right spot and the right time.And you don’t need money for that!" +
+                        "<h4> What is the value of your talk, on what do you want to inspire the listeners?</h4>" +
+                        "The value is that many companies(and developers) are working very traditional: developers build a product, marketing makes a flyer, sales tries to sell it.This is a long process, takes a lot of time and is not fun at all.I want to show you can do it differently: if the developer is involved very early with the marketing working together, you can adapt quickly, support your early users and make a product super big with a lot of users within no - time.But one of the most important things; a growth hack team NEEDS a developer.I want to show the role of the developer here." +
+                        "<h4> What will your listeners take for themselves from your talk ?</h4>" +
+                        "On a tech conference most talks are about how to update x or how to build x in javascript.This talk is different.It shows a new way of working, a new role as a developer, and it will be done with a lot of real life examples and case studies." +
+                        "<h4> What will listeners take:</h4>" +
+                        "Listeners will take away that they as a developer are a huge part of the success of the company, and how they can make their company / idea really big with no budget and no expensive sales people.",
+
                     lang: "[EN]"
                 }
             ],
@@ -80,8 +98,158 @@ $(document).ready(function () {
                     fatype: 'github'
                 },
             ],
+        },
+        {
+            image: "img-min/2020/speakers/joel_lord.jpg",
+            name: "Joel Lord",
+            speakerPos: [
+                {
+                    position: "Developer Advocate",
+                    company: "Red Hat OpenShift",
+                },
+            ],
+            country: "Canada",
+            city: "Toronto",
+            rept: [
+                {
+                    title: "Kube Restaraunt",
+                    description: "You’ve been to a restaurant before, right? Have you noticed how everything is structured? Everyone has a job and there is a chain of command. You can’t just walk into the kitchen and tell the cook how you want your steak done.  Kubernetes is actually very similar to this. You will deploy some pods that do specific things and set the rules of communications that dictate how they all talk to each other. By expanding on the idea of a restaurant, the speaker will slowly introduce some of the basic concepts behind modern DevOps in a fun and easy to understand way. The attendees will come out of this talk with a whole new perspective on what Kubernetes does and how to apply this in their software development practices.",
+                    lang: "[EN]"
+                }
+            ],
+            aboutSpeaker: '',
+            socialsRendered: '',
+            socials: [
+                {
+                    link: 'https://www.linkedin.com/in/joel-lord/',
+                    fatype: 'linkedin'
+                },
+                {
+                    link: 'https://twitter.com/joel__lord',
+                    fatype: 'twitter'
+                },
+                {
+                    link: 'https://github.com/joellord',
+                    fatype: 'github'
+                },
+            ],
+        },
+        {
+            image: "img-min/2020/speakers/yoni_goldberg.jpg",
+            name: "Yoni Goldberg",
+            speakerPos: [
+                {
+                    position: "Architect Consultant",
+                    company: "Yoni Goldberg",
+                },
+            ],
+            country: "Israel",
+            city: "Haifa",
+            rept: [
+                {
+                    title: "TESTING IN PRODUCTION - A PRACTICAL OVERVIEW WITH LIVE EXAMPLES",
+                    description: "This is probably a mega-trend in testing which is covered in many 2020 conferences. It doesn't mean simply testing over production rather a set of techniques that makes the tests more realistic. Most talks on this matter are theoretic, mine aims to bring this fascinating trend down to earth and show live how Node.js code and test runners can discover severe issues that couldn't be discovered using traditional testing. Specifically, we're about to explore techniques like tap-compare, silent releases, traffic shadowing and more",
+                    lang: "[EN]"
+                }
+            ],
+            aboutSpeaker: '',
+            socialsRendered: '',
+            socials: [
+                {
+                    link: 'https://www.linkedin.com/in/yoni-goldberg-760898b/',
+                    fatype: 'linkedin'
+                },
+                {
+                    link: 'https://twitter.com/goldbergyoni',
+                    fatype: 'twitter'
+                },
+                {
+                    link: 'https://github.com/goldbergyoni',
+                    fatype: 'github'
+                },
+                {
+                    link: 'https://goldbergyoni.com/',
+                    fatype: 'link'
+                }
+            ],
+        },
+        {
+            image: "img-min/2020/speakers/aleksandr_korotaev.jpg",
+            name: "Aleksandr Korotaev",
+            speakerPos: [
+                {
+                    position: "Front-End developer",
+                    company: "Tinkoff",
+                },
+            ],
+            country: "Russia",
+            city: "Saint-Petersburg",
+            rept: [
+                {
+                    title: "GIF IS DEAD. WHO'S NEXT?",
+                    description: "It is a talk about animated images formats that went after GIF.Which one will be better: mp4, webm, apng, sprites or vector- based ? Actually no right answer.I want to cover this topic from different sides: HTML app, WebGL game and how to automate preparing of animations for different platforms." +
+                        "<h4> What will your listeners take for themselves from your talk?</h4>" +
+                        "Much of knowledge I want to cover in this topic currently not presented in well - known docs or caniuse.com.I am not focused on 'good story about the future'.It's a mostly practical valued talk to solve this question here and now. I'll cover animations topic from different sides: how the developer can produce own script to convert frames from designers, how to deliver different formats for browsers and exactly much more disadvantages of working with videos"+
+                            "<h4> What so special your talk has, that is absent in the documentation ?</h4>" +
+                            "<ul>" +
+                            "<li> different animation formats for web</li>" +
+                            "<li> which format has a better balance between supporting, size and quality</li>" +
+                            "<li> preparing of videos for different platforms using ffmpeg and automation of it</li>" +
+                            "<li> problems of using of videos in WebGL applications</li>" +
+                            "<li> how to make cross - platform transparent video</li>" +
+                            "<ul>",
+                    lang: "[EN]"
+                }
+            ],
+            aboutSpeaker: '',
+            socialsRendered: '',
+            socials: [
+                {
+                    link: 'https://www.linkedin.com/in/lekzd/',
+                    fatype: 'linkedin'
+                },
+                {
+                    link: 'https://twitter.com/mamu_eval',
+                    fatype: 'twitter'
+                },
+                {
+                    link: 'https://github.com/lekzd',
+                    fatype: 'github'
+                }
+            ],
+        },
+        {
+            image: "img-min/2020/speakers/viktor_turskyi.jpg",
+            name: "Viktor Turskyi",
+            speakerPos: [
+                {
+                    position: "CEO and Solution architect",
+                    company: "WebbyLab",
+                },
+            ],
+            country: "Ukraine",
+            city: "Kyiv",
+            rept: [
+                {
+                    title: "Effective NodeJS Application Development ",
+                    description: "",
+                    lang: "[EN]"
+                }
+            ],
+            aboutSpeaker: '',
+            socialsRendered: '',
+            socials: [
+                {
+                    link: 'https://twitter.com/koorchik',
+                    fatype: 'twitter'
+                },
+                {
+                    link: 'https://github.com/koorchik',
+                    fatype: 'github'
+                },
+            ],
         }
-        
+
 
     ];
 
